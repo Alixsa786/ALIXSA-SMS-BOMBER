56c8afb1055dff0f49df21805dfb6db282f70e65
Alixsa786 <127761147+Alixsa786@users.noreply.github.com>
2023-03-18 19:03:34 +0600

    Add files via upload

---
+++ UPMax.py
@@ -0,0 +1,15 @@
+# HELLO WORLD!😈
+# TRY TO DECRYPT😏
+# encrypted by Mir Sakil Ahmed XS
+
+
+import os,time
+os.system("clear")
+print("\n\033[38;5;195m[\033[38;5;46m+\033[38;5;195m]\033[38;5;46m CHECKING UPDATE 💚")
+time.sleep(2)
+os.system("git pull")
+time.sleep(1)
+os.system("clear")
+os.system("xdg-open https://www.facebook.com/groups/414110602675291/?ref=share")
+
+import ali
